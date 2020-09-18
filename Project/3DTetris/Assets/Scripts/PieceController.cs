0845417517af78e761f45b8b68da8705456ac271
helomdrs <heloisam.silva@hotmail.com>
2020-09-18 18:03:44 -0300

    Cenário e movimentação inicial

---
+++ Project/3DTetris/Assets/Scripts/PieceController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceController : MonoBehaviour
+{
+    public float addFallSpeed = 0f;
+    public float fallSpeed = 1f;
+
+    StageGrid grid;
+    bool isValidPosition;
+
+    void Start()
+    {
+        grid = GameObject.FindGameObjectWithTag("StageGrid").GetComponent<StageGrid>();
+    }
+
+    void Update()
+    {
+        if (IsValidPosition())
+            transform.position += new Vector3(0, -fallSpeed, 0);
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (IsValidPosition())
+                transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (IsValidPosition())
+                transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (IsValidPosition())
+                transform.position += new Vector3(0, -(fallSpeed + addFallSpeed), 0);
+        }
+        //Piece Rotation
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (IsValidPosition())
+                transform.Rotate(0, 0, 90);
+        }
+    }
+
+    bool IsValidPosition()
+    {
+        Vector2 pos = grid.Round(transform.position);
+
+        if (grid.IsInsideGrid(pos))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # 3DTetris
-A 3D Tetris game for Kokku's Junior Developer recruitment process using Unity Engine 2020.1f
-Development made based in [SAMAC Studios](https://www.youtube.com/watch?v=yLNiTi2j0I0&ab_channel=SAMACStudios)
+A 3D Tetris game for Kokku's Junior Developer recruitment process using Unity Engine 2020.1.3.f1
 
-##Assets
+Inspirated by [JEON SOYEON Jelly](https://www.youtube.com/watch?v=FJ8kJdTYj1o) that I was listening when I started this!
+
+Development made based in [The Weekly Coder](https://www.youtube.com/playlist?list=PLiRrp7UEG13axMHD7Kqdiy30c7ZBu_Zn7)
+
+## Assets
 - 3D Tetris pieces made in [MagicaVoxel](https://ephtracy.github.io/)
 - Softbody effect made by [NVIDIA FleX for Unity](https://assetstore.unity.com/packages/tools/physics/nvidia-flex-for-unity-1-0-beta-120425)
+- Voxel Low Poly Forniture by [VIRTI](https://assetstore.unity.com/packages/3d/props/furniture/voxel-furniture-free-170365)
+- UI Packs by [Kenney Assets](https://www.kenney.nl/assets?q=ui)
 - Sound Effects by...
 
