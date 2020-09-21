4cd35cd6765c81a5dc9ad0267b0b479b2b834125
helomdrs <heloisam.silva@hotmail.com>
2020-09-20 21:04:24 -0300

    Tela inicial básica

---
+++ 3D Tetris/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject menuPanel, creditsPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+}
